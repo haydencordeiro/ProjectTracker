604710b57e4674b60d60e6aff05d50daaded14ee
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-11-30 13:09:47 -0500

    Changes to add more task list

--- src/components/AddTaskList.jsx
+++ src/components/AddTaskList.jsx
@@ -1,46 +1,65 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { FaPlus } from 'react-icons/fa';
 import { HiDotsHorizontal } from 'react-icons/hi';
 import { BsClock } from 'react-icons/bs';
 import { MdClose } from "react-icons/md";
 
 
-const AddTaskList = () => {
-    const [addingNewTask, setAddingNewTaskList] = useState(false)
-    const [inputText, setInputText] = useState('')
-  return (
-    <div className={`flex flex-row h-10 w-64 mt-2 rounded-md bg-addNewListBg items-center hover:bg-transparent`}
-    onClick={() => {setAddingNewTaskList(true)}} >
-      <FaPlus className='rounded-sm mx-2 ml-4 text-addNewListText text-sm' />
-      <h2 className='text-sm text-addNewListText p-2'>Add a card</h2>
 
-    {/* <input
-          className={`${addingNewTask ? 'block p-2 pb-8' : 'hidden'} bg-taskCardBg outline-none text-taskCardText`}
-          placeholder='Enter a title for this card...'
-          value={inputText}
-          addingnewtask={addingNewTask.toString()}
-          onChange={event => setInputText(event.target.value)}
-          onKeyDown={event => {
-            if (event.key === 'Enter') {
-              AddCard({ 'list': taskListName, 'task': inputText });
-              setAddingNewTask(false);
-            }
-          }}
-        /> */}
-      {/* {addingNewTask ?
-        <div className='bg-cyan-500 flex items-center hover:bg-red-500 mx-1 rounded-md text-whiteText' >
-          <h2 className='text-sm text-navbarBackground font-semibold rounded-sm py-1 px-2 bg-menuButtonColor'
-            onClick={() => { setAddingNewTaskList(false); }}>
+
+const AddTaskList = ({AddNewTaskList}) => {
+  const [addingNewTask, setAddingNewTaskList] = useState(false)
+  const [inputText, setInputText] = useState('')
+
+
+
+  const AddingTaskList = () => {
+    return (
+      <div className='flex flex-col h-20 bg-navbarBackground w-64 rounded-md mt-2'>
+        <div
+          className='flex flex-col bg-taskCardBg rounded-lg m-1 h-10'
+        >
+          <input
+            className='block p-2 pb-3 bg-taskCardBg outline-none text-taskCardText'
+            placeholder='Enter a title for this card...'
+            value={inputText}
+            addingnewtask={addingNewTask.toString()}
+            onChange={event => setInputText(event.target.value)}
+            onKeyDown={event => {
+              if (event.key === 'Enter') {
+                AddNewTaskList(inputText)
+                setAddingNewTaskList(false);
+                setInputText('')
+              }
+            }}
+          />
+        </div>
+
+        <div className='flex items-center mx-1 rounded-md text-whiteText' >
+          <h2 className='text-sm text-navbarBackground font-semibold rounded-sm py-1 px-2 p-3 mb-1 bg-menuButtonColor'
+            onClick={() => { setAddingNewTaskList(false); AddNewTaskList(inputText); setInputText('') }}>
             Add card</h2>
           <MdClose className='rounded-sm mx-2 text-taskCardText text-lg z-10' onClick={() => { setInputText(''); setAddingNewTaskList(false) }} />
-        </div> :
-        <div className='bg-cyan-500 flex items-center hover:bg-red-500 mx-1 rounded-md text-whiteText hover:bg-backgroundNeutralHovered'
-        onClick={() => { setInputText(''); setAddingNewTaskList(true) }} >
-          <FaPlus className='rounded-sm mx-2 text-taskCardText text-sm' />
-          <h2 className='text-sm text-taskCardText p-2'>Add a card</h2>
         </div>
-      } */}
-    </div>  )
+      </div>
+    )
+  }
+
+  return (
+
+
+    addingNewTask ?
+      AddingTaskList() :
+
+
+
+      <div className={`flex flex-row h-10 w-64 mt-2 rounded-md bg-addNewListBg items-center hover:bg-transparent`}
+        onClick={() => { setAddingNewTaskList(true) }} >
+        <FaPlus className='rounded-sm mx-2 ml-4 text-addNewListText text-sm' />
+        <h2 className='text-sm text-addNewListText p-2'>Add a card</h2>
+      </div >
+
+  )
 }
 
 export default AddTaskList
--- src/components/BoardMainComponent.jsx
+++ src/components/BoardMainComponent.jsx
@@ -8,98 +8,104 @@ const BoardMainComponent = () => {
   const [allTaskList, setAllTaskList] = useState(
     [
       {
-          "task": "Complete Project Proposal",
-          "dueDate": "25 Nov",
-          "list": "ToDo",
-          "id": "aa0a27c7-24e1-4a42-8bd5-3fda242378ea"
+        "task": "Complete Project Proposal",
+        "dueDate": "25 Nov",
+        "list": "ToDo",
+        "id": "aa0a27c7-24e1-4a42-8bd5-3fda242378ea"
       },
       {
-          "task": "Grocery Shopping",
-          "dueDate": "26 Nov",
-          "list": "ToDo",
-          "id": "2d9ec1bf-becf-4f52-b6a6-8a961d8b4b84"
+        "task": "Grocery Shopping",
+        "dueDate": "26 Nov",
+        "list": "ToDo",
+        "id": "2d9ec1bf-becf-4f52-b6a6-8a961d8b4b84"
       },
       {
-          "task": "Send Birthday Gift",
-          "dueDate": "30 Nov",
-          "list": "ToDo",
-          "id": "8a78b4c1-6c90-44a5-9b4d-dae5ec72b54a"
+        "task": "Send Birthday Gift",
+        "dueDate": "30 Nov",
+        "list": "ToDo",
+        "id": "8a78b4c1-6c90-44a5-9b4d-dae5ec72b54a"
       },
       {
-          "task": "Review Meeting Notes",
-          "dueDate": "28 Nov",
-          "list": "InProgress",
-          "id": "0f81c84e-8b3d-499f-9c9b-894f7f5095a8"
+        "task": "Review Meeting Notes",
+        "dueDate": "28 Nov",
+        "list": "InProgress",
+        "id": "0f81c84e-8b3d-499f-9c9b-894f7f5095a8"
       },
       {
-          "task": "Submit Expense Report",
-          "dueDate": "02 Dec",
-          "list": "InProgress",
-          "id": "f2c4e929-d9b0-4c88-9379-615607c6d1c8"
+        "task": "Submit Expense Report",
+        "dueDate": "02 Dec",
+        "list": "InProgress",
+        "id": "f2c4e929-d9b0-4c88-9379-615607c6d1c8"
       },
       {
-          "task": "Read Chapter 5",
-          "dueDate": "01 Dec",
-          "list": "Completed",
-          "id": "501bf538-15d2-4db2-a4d5-d08d392c5a48"
+        "task": "Read Chapter 5",
+        "dueDate": "01 Dec",
+        "list": "Completed",
+        "id": "501bf538-15d2-4db2-a4d5-d08d392c5a48"
       },
       {
-          "task": "Call Mom",
-          "dueDate": "29 Nov",
-          "list": "Completed",
-          "id": "0a86188a-3d3d-43b1-bc0c-56516a686a74"
+        "task": "Call Mom",
+        "dueDate": "29 Nov",
+        "list": "Completed",
+        "id": "0a86188a-3d3d-43b1-bc0c-56516a686a74"
       },
       {
-          "task": "Plan Weekend Trip",
-          "dueDate": "27 Nov",
-          "list": "Pending",
-          "id": "f41c0c2a-858e-4274-b1b8-4322e01e63c4"
+        "task": "Plan Weekend Trip",
+        "dueDate": "27 Nov",
+        "list": "Pending",
+        "id": "f41c0c2a-858e-4274-b1b8-4322e01e63c4"
       },
       {
-          "task": "Exercise Routine",
-          "dueDate": "22 Nov",
-          "list": "Pending",
-          "id": "aeb2a394-b13a-4e6c-8e54-68e98296d4ef"
+        "task": "Exercise Routine",
+        "dueDate": "22 Nov",
+        "list": "Pending",
+        "id": "aeb2a394-b13a-4e6c-8e54-68e98296d4ef"
       },
       {
-          "task": "Learn New Recipe",
-          "dueDate": "03 Dec",
-          "list": "ToDo",
-          "id": "9a2ff95a-98f2-4da0-918d-6f1f43e3b61d"
+        "task": "Learn New Recipe",
+        "dueDate": "03 Dec",
+        "list": "ToDo",
+        "id": "9a2ff95a-98f2-4da0-918d-6f1f43e3b61d"
       }
-  ]
-  
+    ]
+
   )
 
 
   const dragRef = useRef({
     dragTask: 0,
     draggeOverTask: 0,
-    dragListName:"",
-    draggedOverListName:"",
+    dragListName: "",
+    draggedOverListName: "",
   });
-  
-  
+
+
   const [taskList, setTaskList] = useState(['ToDo', 'InProgress', 'Pending'])
 
+
+  const AddNewTaskList = (newTaskListName) => {
+    let temp = [...taskList]
+    temp.push(newTaskListName)
+    setTaskList(temp)
+  }
   function handleSort(isEntireList = false) {
-    const allTaskListClone = isEntireList ? [...taskList] :  [...allTaskList];
+    const allTaskListClone = isEntireList ? [...taskList] : [...allTaskList];
     let firstObjectIndex;
     let secondObjectIndex
-    if(isEntireList){
+    if (isEntireList) {
       // dragTitleListName -> Todo List Name Picked up
       // draggedOverTitleListName -> ToDo List Name to Swap with
-      firstObjectIndex= allTaskListClone.findIndex(obj => obj === dragRef.current.dragTitleListName);
+      firstObjectIndex = allTaskListClone.findIndex(obj => obj === dragRef.current.dragTitleListName);
       secondObjectIndex = allTaskListClone.findIndex(obj => obj === dragRef.current.draggedOverTitleListName);
     }
-    else{
+    else {
       firstObjectIndex = allTaskListClone.findIndex(obj => obj.id === dragRef.current.dragTask);
       secondObjectIndex = allTaskListClone.findIndex(obj => obj.id === dragRef.current.draggeOverTask);
     }
 
     // swapping logic
     let temp = allTaskListClone[firstObjectIndex];
-    if(!isEntireList){
+    if (!isEntireList) {
       // change the list for dragged task
       temp.list = dragRef.current.draggedOverListName
     }
@@ -110,16 +116,16 @@ const BoardMainComponent = () => {
     // updating the appropriate list
     isEntireList ? setTaskList(allTaskListClone) : setAllTaskList(allTaskListClone);
   }
-  
 
-  function AddCard(taskData){
+
+  function AddCard(taskData) {
     const temp = [...allTaskList]
     taskData['id'] = "9a2ff95a-98f2-4da0-918d-6f1f43e3b61dd" + taskData.task
     temp.push(
       taskData
     )
     setAllTaskList(temp)
-    
+
   }
   return (
     <div className="h-screen bg-green bg-fit bg-center bg-[url('https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2048x1194/1ae72d8a416e9a846331da7083f0d4ba/photo-1694250990115-ca7d9d991b24.jpg')]">
@@ -128,10 +134,12 @@ const BoardMainComponent = () => {
       <div className='flex overflow-auto h-[559px]'>
         {taskList.map((task, index) =>
           <TaskList taskListName={task} tasks={allTaskList} key={index} ref={dragRef} handleSort={handleSort}
-          AddCard={AddCard}
+            AddCard={AddCard}
           ></TaskList>
         )}
-        <AddTaskList></AddTaskList>
+        <AddTaskList
+          AddNewTaskList={AddNewTaskList}
+        ></AddTaskList>
 
 
       </div>
